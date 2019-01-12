34179ab137f8a38b7038f6c63bf69043a97674c3
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2019-01-12 11:29:31 -0800

    Remove hardcoded DIFF_ID used for testing.

--- scripts/visual-testing-diffy.sh
+++ scripts/visual-testing-diffy.sh
@@ -6,8 +6,7 @@ echo -e "\nRunning visual regression tests for $SITE_NAME with UUID $SITE_UUID..
 
 TOKEN=`curl -X POST "https://diffy.website/api/auth/key" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"key\":\"${DIFFY_KEY}\"}" | php -r 'echo json_decode(file_get_contents("php://stdin"))->token;'`
 
-#DIFF_ID=`curl -X POST "https://diffy.website/api/projects/${DIFFY_PROJECT}/compare" -H "accept: application/json" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"environments\":\"custom\",\"url1\":\"${LIVE_URL}\",\"url2\":\"${MULTIDEV_URL}\"}" | php -r 'echo str_replace("diff: ", "", json_decode(file_get_contents("php://stdin")));'`
-DIFF_ID=3863
+DIFF_ID=`curl -X POST "https://diffy.website/api/projects/${DIFFY_PROJECT}/compare" -H "accept: application/json" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"environments\":\"custom\",\"url1\":\"${LIVE_URL}\",\"url2\":\"${MULTIDEV_URL}\"}" | php -r 'echo str_replace("diff: ", "", json_decode(file_get_contents("php://stdin")));'`
 
 DIFF_INFO=`curl -X GET "https://diffy.website/api/diffs/${DIFF_ID}" -H "accept: application/json" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"`
 DIFF_URL=`echo "${DIFF_INFO}" | php -r 'echo json_decode(file_get_contents("php://stdin"))->archiveUrl;'`
